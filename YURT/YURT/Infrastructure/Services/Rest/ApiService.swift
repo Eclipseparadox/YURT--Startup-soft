6925c950a16c3d70a68b10a6aaded63bb1ab3de9
Standret <pstandret@gmail.com>
2018-05-12 20:26:21 +0300

    add realm and start working with dal

---
+++ YURT/YURT/Infrastructure/Services/Rest/ApiService.swift
@@ -0,0 +1,33 @@
+//
+//  ApiService.swift
+//  YURT
+//
+//  Created by Standret on 03.05.18.
+//  Copyright © 2018 com.yurt.YURT. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+import KeychainSwift
+
+protocol IApiService {
+    func inserToken(token: String)
+}
+
+class ApiService: IApiService {
+    
+    var _httpService: IHttpService!
+    
+    init() {
+      //  ServiceInjectorAssembly.instance().inject(into: self)
+        _httpService.url = Constants.apiUrl
+        _httpService.token = KeychainSwift().get(Constants.tokenKey)
+    }
+    
+    func inserToken(token: String) {
+        _httpService.token = token
+    }
+    
+    
+}
