74e67647c9163667419bd14ce736aac3cb7e554c
Standret <pstandret@gmail.com>
2018-05-29 16:07:50 +0300

    completed first step taken photo

---
+++ YURT/YURT/Views/ViewControllers/TakingPhoto/PreviewPhotoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PreviewPhotoViewController.swift
+//  YURT
+//
+//  Created by Standret on 29.05.18.
+//  Copyright © 2018 com.yurt.YURT. All rights reserved.
+//
+
+import UIKit
+
+class PreviewPhotoViewController: UIViewController {
+    
+    var image: UIImage!
+    var delegate: SttTakePhotoDelegate!
+
+    @IBOutlet weak var imgPhoto: UIImageView!
+    
+    @IBAction func cancelClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        delegate.close(isUsePhoto: false)
+    }
+    @IBAction func usePhotoClick(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        delegate.close(isUsePhoto: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imgPhoto.image = image
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- YURT/YURT/Views/ViewControllers/TakingPhoto/TakePhotoViewController.swift
+++ YURT/YURT/Views/ViewControllers/TakingPhoto/TakePhotoViewController.swift
@@ -10,21 +10,62 @@ import Foundation
 import AVFoundation
 import UIKit
 
-class TakePhotoViewController: SttViewController<TakePhotoPresenter>, TakePhotoDelegate {
+protocol SttTakePhotoDelegate: class {
+    func close(isUsePhoto: Bool)
+}
+
+class TakePhotoViewController: SttViewController<TakePhotoPresenter>, TakePhotoDelegate, AVCapturePhotoCaptureDelegate, SttTakePhotoDelegate {
     
     let captureSession = AVCaptureSession()
+    
     var previewLayer: CALayer!
     
     var captureDevice: AVCaptureDevice!
+    var photoOutput: AVCapturePhotoOutput?
+    var takedPhoto: UIImage!
+    
+    @IBOutlet weak var btnTakePhoto: UIButton!
+    @IBAction func takePhotoClick(_ sender: Any) {
+        let settings = AVCapturePhotoSettings()
+        photoOutput?.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation() {
+            takedPhoto = UIImage(data: imageData)
+            performSegue(withIdentifier: "previewPhoto", sender: nil)
+        }
+    }
+    @IBAction func cancelClick(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        prepareCamer()
+        btnTakePhoto.createCircle()
     }
     
-    @IBAction func cancelClick(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    private var fistStart = true
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if fistStart {
+            fistStart = false
+            prepareCamer()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "previewPhoto" {
+            let previewC = segue.destination as! PreviewPhotoViewController
+            previewC.image = takedPhoto
+            previewC.delegate = self
+        }
+    }
+    
+    func close(isUsePhoto: Bool) {
+        if isUsePhoto {
+            dismiss(animated: true, completion: nil)
+        }
     }
     
     func prepareCamer() {
@@ -41,6 +82,9 @@ class TakePhotoViewController: SttViewController<TakePhotoPresenter>, TakePhotoD
         do {
             let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
             captureSession.addInput(captureDeviceInput)
+            photoOutput = AVCapturePhotoOutput()
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+            captureSession.addOutput(photoOutput!)
         }
         catch {
             Log.error(message: "\(error)", key: "Capture error")
